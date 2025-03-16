722383d38f8f19090efac02c232e068098a835f4
YooSeoyoung <yoo_seoyoung@naver.com>
2025-03-17 00:07:11 +0900

    .

--- src/main/java/com/dw/TheBoxer/model/Tag.java
+++ src/main/java/com/dw/TheBoxer/model/Tag.java
@@ -14,10 +14,9 @@ import java.util.List;
 @Entity
 @Table(name = "tag")
 public class Tag {
-
-
     @Id
     private String name;  // 태그 이름 (예: '복싱화', '샌드백', '루틴', '식단' 등 관리자가  고정)
+
 
     @OneToMany(mappedBy = "tag")  // 하나의 태그는 여러 게시글을 가질 수 있음
     private List<Board> boards = new ArrayList<>();  // 해당 태그가 포함된 게시글 목록
--- src/main/java/com/dw/TheBoxer/model/UserProfile.java
+++ src/main/java/com/dw/TheBoxer/model/UserProfile.java
@@ -23,6 +23,16 @@ public class UserProfile {
     @JoinColumn(name = "user_name")
     private User user; // 하나의 유저가 많은 userProfile을 가질 수 있음
 
+    @ManyToMany
+    @JoinTable(
+        name = "user_profile_tag",
+        joinColumns = @JoinColumn(name = "user_profile_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<Tag> preferredTags = new ArrayList<>();  // 유저가 선호하는 태그 목록
+    //하나의 유저가 여러 태그를 선호할 수 있고, 동시에 하나의 태그가 여러 유저에게 선호될 수 있음
+}
+
 //    @Column(name = "preferred_equipment")
 //    private String preferredEquipment ="";  // 선호 장비 (예: 복싱 글러브, 샌드백 등) -> board에 title 기준
 //
@@ -34,12 +44,3 @@ public class UserProfile {
 //
 //    @Column(name = "preferred_routine")
 //    private String preferredRoutine ="";  //-> board에 category 기준(루틴)
-@ManyToMany
-@JoinTable(
-        name = "user_profile_tag",
-        joinColumns = @JoinColumn(name = "user_profile_id"),
-        inverseJoinColumns = @JoinColumn(name = "tag_id")
-)
-private List<Tag> preferredTags = new ArrayList<>();  // 유저가 선호하는 태그 목록
-    //하나의 유저가 여러 태그를 선호할 수 있고, 동시에 하나의 태그가 여러 유저에게 선호될 수 있음
-}
