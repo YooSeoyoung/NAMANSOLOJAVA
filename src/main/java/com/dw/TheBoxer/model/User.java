0398e59cd1e36c51abe2132da436a543937127b7
JJ0824 <auxjungjae@gmail.com>
2025-03-24 17:13:59 +0900

    Update User.java

--- src/main/java/com/dw/TheBoxer/model/User.java
+++ src/main/java/com/dw/TheBoxer/model/User.java
@@ -9,33 +9,48 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
 @ToString
 @Entity
 @Table(name = "user")
 public class User {
     @Id
+    @Setter
     @Column(name="user_name")
     private String userName; // 유저명
 
+    @Setter
     @Column(name="password", nullable = false)
     private String password; // 비밀번호
 
+    @Setter
     @Column(name = "real_name_m", nullable = false)
     private String realNameM; // 남자 이름
 
+    @Setter
     @Column(name = "real_name_f", nullable = false)
     private String realNameF; // 여자 이름
 
+    @Setter
     @Column(name="email_m", nullable = false)
     private String emailM; // 남자 이메일
 
+    @Setter
     @Column(name="email_f", nullable = false)
     private String emailF; // 여자 이메일
 
+    @Setter
+    @Column(name = "birth_m", nullable = false)
+    private LocalDate birthM; // 남자 생일
+
+    @Setter
+    @Column(name = "birth_f", nullable = false)
+    private LocalDate birthF; // 여자 생일
+
+    @Setter
     @Column(name = "phone_number_m", nullable = false)
     private String phoneNumberM; // 남자 번호
 
+    @Setter
     @Column(name = "phone_number_f", nullable = false)
     private String phoneNumberF; // 여자 번호
 
