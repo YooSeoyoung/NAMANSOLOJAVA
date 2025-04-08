b03fd15e515e8f72ef66c1c144366527f9f39489
JJ0824 <auxjungjae@gmail.com>
2025-04-08 23:37:27 +0900

    팔로우 업데이트

--- src/main/java/com/dw/NAMANSOLOJAVA/Controller/FollowController.java
+++ src/main/java/com/dw/NAMANSOLOJAVA/Controller/FollowController.java
@@ -1,9 +1,15 @@
 package com.dw.NAMANSOLOJAVA.Controller;
 
+import com.dw.NAMANSOLOJAVA.DTO.FollowDTO;
+import com.dw.NAMANSOLOJAVA.DTO.FollowerDTO;
+import com.dw.NAMANSOLOJAVA.DTO.FollowingDTO;
 import com.dw.NAMANSOLOJAVA.Service.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/follow")
@@ -11,5 +17,35 @@ public class FollowController {
     @Autowired
     FollowService followService;
 
+    @GetMapping("/all")
+    private ResponseEntity<List<FollowDTO>> getAllFollow() {
+        return new ResponseEntity<>(followService.getAllFollow(), HttpStatus.OK);
+    }
 
+    @PostMapping("/new/following")
+    private ResponseEntity<FollowingDTO> saveNewFollowing(@RequestBody FollowingDTO followingDTO) {
+        return new ResponseEntity<>(followService.saveNewFollowing(followingDTO), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/new/follower")
+    private ResponseEntity<FollowerDTO> saveNewFollower(@RequestBody FollowerDTO followerDTO) {
+        return new ResponseEntity<>(followService.saveNewFollower(followerDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all/{username}")
+    private ResponseEntity<List<FollowDTO>> getAllFollowByUsername(@PathVariable String username) {
+        return new ResponseEntity<>(followService.getAllFollowByUsername(username), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/following/{id}")
+    private ResponseEntity<String> deleteFollowing(@PathVariable Long id) {
+        return new ResponseEntity<>(followService.deleteFollowing(id), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/delete/follower/{id}")
+    private ResponseEntity<String> deleteFollower(@PathVariable Long id) {
+        return new ResponseEntity<>(followService.deleteFollower(id), HttpStatus.ACCEPTED);
+    }
+
+//    @PutMapping("/")
 }
--- src/main/java/com/dw/NAMANSOLOJAVA/Service/FollowService.java
+++ src/main/java/com/dw/NAMANSOLOJAVA/Service/FollowService.java
@@ -1,11 +1,51 @@
 package com.dw.NAMANSOLOJAVA.Service;
 
+import com.dw.NAMANSOLOJAVA.DTO.FollowDTO;
+import com.dw.NAMANSOLOJAVA.DTO.FollowerDTO;
+import com.dw.NAMANSOLOJAVA.DTO.FollowingDTO;
 import com.dw.NAMANSOLOJAVA.Repository.FollowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Service
 public class FollowService {
     @Autowired
     FollowRepository followRepository;
+
+    public List<FollowDTO> getAllFollow() {
+        return null;
+//        return followRepository.getAllFollower();
+    }
+
+    public FollowingDTO saveNewFollowing(FollowingDTO followingDTO) {
+        return null;
+//        return followRepository.saveNewFollowing(followingDTO);
+    }
+
+    public FollowerDTO saveNewFollower(FollowerDTO followerDTO) {
+        return null;
+//        return followRepository.saveNewFollower(followerDTO);
+    }
+
+    public List<FollowDTO> getAllFollowByUsername(String username) {
+        return null;
+//        return followRepository.getAllFollowByUsername(username);
+    }
+
+    public String deleteFollowing(Long id) {
+        return null;
+//        return followRepository.deleteFollowing(id);
+    }
+
+    public String deleteFollower(Long id) {
+        return null;
+//        return followRepository.deleteFollower(id);
+    }
+
+
 }
