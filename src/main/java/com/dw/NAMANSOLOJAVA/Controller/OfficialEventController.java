162e61c183dd5b4adc859ebb5f5b04c1a4897f9c
JJ0824 <auxjungjae@gmail.com>
2025-04-09 17:27:00 +0900

    ㅇㄷㅇㅌ

--- src/main/java/com/dw/NAMANSOLOJAVA/Controller/OfficialEventController.java
+++ src/main/java/com/dw/NAMANSOLOJAVA/Controller/OfficialEventController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-@RestController("/api/officialevent")
+@RestController
+@RequestMapping("/api/officialevent")
 public class OfficialEventController {
     @Autowired
     OfficialEventService officialEventService;
