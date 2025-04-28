7f6e9cb0d3e6df5b6e8990d4f7085abd24307d20
NBear32 <hansel32153@gmail.com>
2025-04-28 18:47:53 +0900

    .

--- src/main/java/com/dw/NAMANSOLOJAVA/Config/WebSocketConfig.java
+++ src/main/java/com/dw/NAMANSOLOJAVA/Config/WebSocketConfig.java
@@ -11,12 +11,12 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker  //WebSocket 메시징을 활성화하는 역할
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired
     private JwtChannelInterceptor jwtChannelInterceptor;
 
-    // stomp의 앤드포인트 설정
+    // stomp(Simple Text Oriented Messaging Protocol)의 앤드포인트 설정
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
@@ -25,6 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .withSockJS();
     }
 
+    //클라이언트에서 보내는 메시지와 서버에서 보내는 메시지를 구분하고 처리할 수 있는 브로커를 설정합니다.
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //브로커(서버) : 받은 내용을 구독을 한 모든 클라이언트에게 전달을 하라는 의미
@@ -36,6 +37,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
     }
 
+    // 클라이언트가 메시지를 보낼 때마다 이 인터셉터가 실행되어,
+    // 해당 메시지가 유효한지 (즉, 인증된 사용자 요청인지) 확인합니다.
+    // 이 인터셉터는 주로 메시지의 헤더에서 JWT 토큰을 검사하는 데 사용됩니다.
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(jwtChannelInterceptor); // 인터셉터 등록
