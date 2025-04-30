cf21f7c9cebfaa4cf3a38386f731198003f3c41d
NBear32 <hansel32153@gmail.com>
2025-04-30 11:33:45 +0900

    공개 여부

--- src/main/java/com/dw/NAMANSOLOJAVA/enums/Visibility.java
+++ src/main/java/com/dw/NAMANSOLOJAVA/enums/Visibility.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Visibility {
     PUBLIC("누구나"),       // 전체 공개
-    PRIVATE("우리만"),      // 나만 보기
-    FOLLOWERS_ONLY("팔로워들과"); // 팔로워만 보기 (추후 구현 대비)
+    PRIVATE("우리만");      // 나만 보기
+//    FOLLOWERS_ONLY("팔로워들과"); // 팔로워만 보기 (추후 구현 대비)
 
     private final String visible;
 }
